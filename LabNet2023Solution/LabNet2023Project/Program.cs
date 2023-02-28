a24f34a78666c7ed3cd868eb71ae220e9f657173
ignacix <ignanj8mail.com>
2023-02-28 12:17:56 -0300

    Se sube código HelloWorld
    Se sube el código junto con los archivos del proyecto y la soution.

---
+++ LabNet2023Solution/LabNet2023Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet2023Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Por favor, Ingrese su nombre.");
+            nombre = Console.ReadLine();
+            Console.Clear();
+            while (nombre == "")
+            {
+                Console.WriteLine("Por favor, Ingrese su nombre.");
+                nombre = Console.ReadLine();
+                Console.Clear();
+            }
+            Console.WriteLine($"Hello, {nombre}!!");
+            Console.ReadLine();
+        }
+    }
+}
